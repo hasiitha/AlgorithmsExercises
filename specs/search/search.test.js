5e7d9ea8e3068ff24ea14ddd8932e05dcc428f5d
Hasitha Bandara <52845256+hasiitha@users.noreply.github.com>
2022-07-10 19:17:23 +0530

    binary sort done

--- specs/search/search.test.js
+++ specs/search/search.test.js
@@ -3,17 +3,41 @@
 //
 // it's up to you what to return if the object isn't found (we're not testing that)
 
+const { divide } = require("lodash");
+
 function linearSearch(id, array) {
   // code goes here
+  for (let i = 0; i < array.length > 0; i++) {
+    if (id == array[i].id) {
+      return array[i];
+    }
+  }
+  return null;
 }
 
 function binarySearch(id, array) {
-  // code goes here
+  let min = 0;
+  let max = array.length - 1;
+  let index;
+  let element;
+
+  while (min <= max) {
+    index = Math.floor((min + max) / 2);
+    element = array[index];
+    if (element.id < id) {
+      min = index + 1;
+    } else if (element.id > id) {
+      max = index - 1;
+    } else {
+      return element;
+    }
+  }
+  return void 0; //undefined
 }
 
 // unit tests
 // do not modify the below code
-test.skip("linear search", function () {
+test("linear search", function () {
   const lookingFor = { id: 5, name: "Brian" };
   expect(
     linearSearch(5, [
@@ -35,7 +59,7 @@ test.skip("linear search", function () {
   ).toBe(lookingFor);
 });
 
-test.skip("binary search", function () {
+test("binary search", function () {
   const lookingFor = { id: 23, name: "Brian" };
   expect(
     binarySearch(23, [
